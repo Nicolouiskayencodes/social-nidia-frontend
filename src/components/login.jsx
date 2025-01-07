0366deeceb525fc40a70a84294de67406d86701a
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2025-01-06 21:31:09 -0800

    Set loading state and auto log in if jwt is set

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,14 +1,33 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
   const username = useRef(null)
   const password = useRef(null)
+  const [loading, setLoading] = useState(true)
   const navigate = useNavigate()
   useEffect(()=>{
-
-  })
+    if (localStorage.getItem("Authorization")){
+      fetch('http://localhost:3000/user', {
+        mode: "cors",
+      method: "GET",
+      headers: { "Content-Type": "application/json",
+      "Authorization": localStorage.getItem("Authorization")},
+      })
+      .then(response => {
+        if (response.status === 200) {
+          setLoading(false)
+          navigate('/home');
+        } else {
+          return setLoading(false)
+        }
+      })
+    } else {
+      setLoading(false)
+    }
+  }, [loading, navigate])
   const submitLogin = async (event) => {
+    setLoading(true)
     event.preventDefault();
     await fetch("http://localhost:3000/login", {
       mode: "cors",
@@ -27,12 +46,14 @@ function Login() {
         localStorage.setItem("Authorization", `Bearer ${response.token}`)
         navigate('/home');
       }
+      setLoading(false)
     })
   }
 
   return(
     <div>
       <div className="title-card"></div>
+      {loading && <p>loading...</p>}
       <div className="login">
         <form>
           <label htmlFor="username">Username: </label><input type="text" id="username" ref={username}></input><br></br>
